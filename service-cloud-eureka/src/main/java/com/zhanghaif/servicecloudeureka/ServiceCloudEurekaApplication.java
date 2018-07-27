b84830ef143183d18fbc9394c51977e656998782
张海峰 <zhanghaifeng@zhanghaifengdeMacBook-Pro.local>
2018-07-27 15:58:41 +0800

    基于Eureka服务发现

--- service-cloud-eureka/src/main/java/com/zhanghaif/servicecloudeureka/ServiceCloudEurekaApplication.java
+++ service-cloud-eureka/src/main/java/com/zhanghaif/servicecloudeureka/ServiceCloudEurekaApplication.java
@@ -2,8 +2,10 @@ package com.zhanghaif.servicecloudeureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceCloudEurekaApplication {
 
 	public static void main(String[] args) {
