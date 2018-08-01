694257f8383eee15215dc2cd3ceb44c14b517d37
张海峰 <zhanghaifeng@zhanghaifengdeMacBook-Pro-2.local>
2018-08-01 16:29:06 +0800

    修改配置

--- service-cloud-mybatis-db/src/main/java/com/zhanghaif/db/App.java
+++ service-cloud-mybatis-db/src/main/java/com/zhanghaif/db/App.java
@@ -7,21 +7,15 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableCaching
 @EnableScheduling
-@MapperScan("org.wcloud.db.task.mapper")
+@MapperScan("com.zhanghaif.db.mapper")
 public class App 
 {
     public static void main( String[] args )
     {
-//    	org.wcloud.db.task.dao
     	SpringApplication.run(App.class, args);
-//        System.out.println( "Hello World!" );
     }
 }
