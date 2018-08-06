d7ec90ed309ff85900f5b53ea62a7185ea4a533a
张海峰 <zhanghaifeng@zhanghaifengdeMacBook-Pro-2.local>
2018-08-06 14:03:25 +0800

    udp的使用

---
+++ service-cloud-eureka-udp/src/main/java/com/zhanghaif/servicecloudeureka/controller/ClientController.java
@@ -0,0 +1,32 @@
+package com.zhanghaif.servicecloudeureka.controller;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class ClientController {
+
+	 private final static int PORT = 8086;
+	    private static final String HOSTNAME = "localhost";
+	    
+	    
+	    public static void main(String[] args) {
+	        //传入0表示让操作系统分配一个端口号
+	        try (DatagramSocket socket = new DatagramSocket(0)) {
+	            socket.setSoTimeout(10000);
+	            InetAddress host = InetAddress.getByName(HOSTNAME);
+	            //指定包要发送的目的地
+	            DatagramPacket request = new DatagramPacket(new byte[1], 1, host, PORT);
+	            //为接受的数据包创建空间
+	            DatagramPacket response = new DatagramPacket(new byte[1024], 1024);
+	            socket.send(request);
+	            socket.receive(response);
+	            String result = new String(response.getData(), 0, response.getLength(), "ASCII");
+	            System.out.println(result);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    
+}
---
+++ service-cloud-eureka-udp/src/main/java/com/zhanghaif/servicecloudeureka/controller/TestController.java
@@ -0,0 +1,33 @@
+package com.zhanghaif.servicecloudeureka.controller;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.util.Date;
+
+public class TestController {
+
+	private final static int PORT = 8086;
+	
+	public static void main(String[] args) {
+		try (DatagramSocket socket = new DatagramSocket(PORT)) {
+            while (true) {
+                try {
+                    DatagramPacket request = new DatagramPacket(new byte[1024], 1024);
+                    socket.receive(request);
+                    
+                    String daytime = new Date().toString();
+                    byte[] data = daytime.getBytes("ASCII");
+                    DatagramPacket response = new DatagramPacket(data, data.length, request.getAddress(), request.getPort());
+                    socket.send(response);
+                    System.out.println(daytime + " " + request.getAddress());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+	}
+	
+}
